6fbadd35d9e305312f6afcf5d7922c0ea78ab863
3038235446 <3038235446@xiaomi.com>
2025-03-17 17:02:05 +0800

    DCN v2 代码注释完善

--- DCN_study/pytorch-deform-conv-v2-master/pytorch-deform-conv-v2-master/scaled_mnist/archs.py
+++ DCN_study/pytorch-deform-conv-v2-master/pytorch-deform-conv-v2-master/scaled_mnist/archs.py
@@ -25,6 +25,8 @@ class ScaledMNISTNet(nn.Module):
         features = []
         inplanes = 1  # 输入通道数，MNIST 数据集是灰度图，所以初始值为 1。
         outplanes = 32  # 第一个卷积层的输出通道数，初始为 32，之后每层翻倍。
+        # 一共四个卷积层，每个卷积层后面跟 BN 和 ReLU
+        # DCN 出现在 args.min_deform_layer 及之后的层中
         for i in range(4):
             if args.deform and args.min_deform_layer <= i + 1:
                 features.append(DeformConv2d(inplanes, outplanes, 3, padding=1, bias=False, modulation=args.modulation))
@@ -42,8 +44,10 @@ class ScaledMNISTNet(nn.Module):
 
     def forward(self, input):
         x = self.features(input)
-        x = self.avg_pool(x)
-        x = x.view(x.shape[0], -1)
-        output = self.fc(x)
+        x = self.avg_pool(x)  # 进行自适应平均池化，输出变为 (batch_size, 256, 1, 1)
+        # view() 是 PyTorch 中的一个张量操作方法，用于 改变张量的形状（Reshape），但不改变数据本身。
+        # 如果不确定某个维度的大小，可以用 -1 让 PyTorch 自动计算
+        x = x.view(x.shape[0], -1)  # 将 (batch_size, 256, 1, 1) 变成 (batch_size, 256)
+        output = self.fc(x)  # 通过全连接层得到最终分类输出
 
         return output
